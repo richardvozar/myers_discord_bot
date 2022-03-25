48a2dac477de0e0928fc06a07b09e90846995242
Svab <49274788+svabbogar@users.noreply.github.com>
2022-03-26 00:01:25 +0100

    Debugging, adapting to the changes of the scraped site

--- main.py
+++ main.py
@@ -6,14 +6,15 @@ from dotenv import load_dotenv
 import requests
 from bs4 import BeautifulSoup
 from discord.ext import tasks, commands
+from datetime import datetime as dt
 
 
 URL = 'https://progameguides.com/dead-by-daylight/dead-by-daylight-codes/'
 BOT_ONLINE = False
 
 # updated date
-old_date_string = ''
-new_date_string = ''
+old_codes = ''
+new_codes = ''
 
 load_dotenv()
 
@@ -24,9 +25,9 @@ client = discord.Client()
 # updated_string --> str | [Updated Feb. 24]
 # codes --> dict | 'CODE': 'Reedem for XX BP'
 def get_codes(updated_string, codes):
-    ret = '__**Dead by Daylight Reedem Codes:**__ '
+    ret = '__**Dead by Daylight Reedem Codes:**__ ['
     ret += updated_string
-    ret += '\n'
+    ret += ']\n'
     for c, bp in codes.items():
         ret += f'*{c}* --> {bp}\n'
     return ret
@@ -40,9 +41,9 @@ async def on_ready():
 
 
 
-@tasks.loop(seconds=600)
+@tasks.loop(seconds=10)
 async def code_update():
-    global BOT_ONLINE, old_date_string, new_date_string
+    global BOT_ONLINE, old_codes, new_codes
 
     page = requests.get(URL)
 
@@ -52,29 +53,47 @@ async def code_update():
     updated_selector = soup.select('div p strong')
     updated_string = str(updated_selector.pop().get_text())
 
-    # changing the updates
-    old_date_string, new_date_string = new_date_string, updated_string
 
     codes_src = soup.select('#dbd-codes-working + ul li')
 
     codes_src = list(codes_src)
 
-    codes = {}
-    for c_src in codes_src:
-        txt = str(c_src.get_text()).split('—')
-        codes[txt[0]] = txt[1]
 
     if BOT_ONLINE:
+
+        # putting codes in the dictionary 'codes'
+        codes = {}
+        for c_src in codes_src:
+            txt = str(c_src.get_text()).split('—')
+            codes[txt[0]] = txt[1]
+
+        # changing the updates
+        old_codes = new_codes
+        #print(f'old_codes={old_codes}  <--')
+
+        # copying key-value pairs from codes, to new_codes
+        new_codes = {}
+        for k, v in codes.items():
+            new_codes[k] = v
+
+
         channel = client.get_channel(947922114516766741)
 
-        if old_date_string == '':
-            old_date_string = updated_string
-            await channel.send(get_codes(updated_string, codes))
+        if old_codes == '':
+            #print('old_codes=""', old_codes, new_codes)
+            date = dt.now().strftime('%Y. %b %m. %X')
+            print('-- log: FIRST RUN: ' + date)
+            await channel.send(get_codes(date, codes))
 
-        if old_date_string == new_date_string:
+        elif old_codes == new_codes:
+            #print('old_codes == new_codes', old_codes, new_codes)
             return
+
         else:
-            await channel.send(get_codes(updated_string, codes))
+            #print('wut')
+            date = dt.now().strftime('%Y. %b %m. %X')
+            print('-- log: NEW CODE: ' + date)
+            await channel.send(get_codes(date, codes))
 
 
 
